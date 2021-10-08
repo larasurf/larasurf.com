9328cc8aa036f0cc37539a5a2e77988386eb16fe
Brice Hartmann <brice@bricehartmann.com>
2021-10-07 22:04:40 -0500

    spacing

--- resources/views/components/heading1.blade.php
+++ resources/views/components/heading1.blade.php
@@ -1,3 +1,3 @@
 <h1 id="{{ $id }}" class="text-3xl font-bold mt-{{ $marginTop }} mb-12">
-    <span class="twa twa-ocean"></span> <a class="header-link" href="#{{ $id }}">{{ $slot }}</a>
+    <a class="header-link" href="#{{ $id }}"><span class="twa twa-ocean"></span> {{ $slot }}</a>
 </h1>
