c2ff6c241799e524c6a000de6513fc39eb3c0e1d
Brice Hartmann <brice@bricehartmann.com>
2021-12-22 23:25:45 -0600

    publish awslocal s3 filesystem changes

--- resources/generate.sh
+++ resources/generate.sh
@@ -462,7 +462,7 @@ EOF
 
   # build the post installation command
 
-  POST_INSTALL_CMD='php artisan larasurf:publish --env-changes --circleci --gitignore --healthcheck'
+  POST_INSTALL_CMD='php artisan larasurf:publish --awslocal --env-changes --circleci --gitignore --healthcheck'
 
   if [[ "$ENVIRONMENTS" == 'local-stage-production' ]] || [[ "$ENVIRONMENTS" == 'local-production' ]] ; then
     POST_INSTALL_CMD="$POST_INSTALL_CMD --cloudformation --proxies"
