d505cc6b502a854824367df6f504d3913ef90d3f
Brice Hartmann <brice@bricehartmann.com>
2022-01-15 14:21:38 -0600

    documentation search proof of concept

--- resources/js/components/DocumentationSearchBar.vue
+++ resources/js/components/DocumentationSearchBar.vue
@@ -11,30 +11,71 @@ export default {
             currentMatch: null,
             totalMatches: null,
             docsHtml: null,
+            searchTerm: '',
         };
     },
     computed: {
     },
     mounted() {
         this.docsHtml = document.querySelector('#documentation-content').innerHTML;
+
+        window.addEventListener('keydown', (e) => {
+            if ((e.ctrlKey || e.metaKey) && e.key === 'f') {
+                e.preventDefault();
+
+                this.$refs.searchRef.focus();
+            }
+        });
     },
     methods: {
-        searchChildNodes(term, nodes) {
+        searchChildNodes(nodes) {
+            if (this.totalMatches === null) {
+                this.totalMatches = 0;
+            }
+
             if (nodes) {
                 for (let i = 0; i < nodes.length; i++) {
                     switch (nodes[i].nodeType) {
                         // element
                         case 1: {
-                            this.searchChildNodes(term, nodes[i].childNodes);
+                            this.searchChildNodes(nodes[i].childNodes);
                             break;
                         }
                         // text
                         case 3: {
-                            if (nodes[i].nodeValue.includes(term)) {
+                            if (nodes[i].nodeValue.includes(this.searchTerm)) {
+                                let replaceHtml = '';
+                                let nodeValue = nodes[i].nodeValue;
+                                let scrollTo = false;
+
+                                while (nodeValue.includes(this.searchTerm)) {
+                                    const index = nodeValue.indexOf(this.searchTerm);
+                                    const strBeforeIndex = nodeValue.substr(0, index);
+                                    this.totalMatches++;
+
+                                    let css = 'search-result';
+
+                                    if (this.totalMatches === this.currentMatch) {
+                                        css += ' current';
+                                        scrollTo = true;
+                                    }
+
+                                    replaceHtml += `${strBeforeIndex}<span id="search-result-${this.totalMatches}" class="${css}">${this.searchTerm}</span>`;
+                                    nodeValue = nodeValue.substr(index + this.searchTerm.length);
+                                }
+
                                 const el = document.createElement('span');
 
-                                el.innerHTML = nodes[i].nodeValue.replaceAll(term, `<span class="search-result">${term}</span>`);;
+                                el.innerHTML = replaceHtml + nodeValue;
                                 nodes[i].replaceWith(el);
+
+                                if (scrollTo) {
+                                    window.scrollTo({
+                                        left: 0,
+                                        top: el.getBoundingClientRect().top + window.pageYOffset - 300,
+                                        behavior: 'smooth',
+                                    })
+                                }
                             }
                             break;
                         }
@@ -46,14 +87,52 @@ export default {
             const docsContent = document.querySelector('#documentation-content');
             docsContent.innerHTML = this.docsHtml;
 
-            if (term && term.length > 2) {
-                this.searchChildNodes(term, docsContent.childNodes);
-            } else {
-                this.currentMatch = null;
-                this.totalMatches = null;
+            this.currentMatch = 1;
+            this.totalMatches = null;
+
+            this.searchTerm = term || '';
+
+            if (this.searchTerm.length > 2) {
+                this.searchChildNodes(docsContent.childNodes);
+            }
+
+            if (!this.totalMatches) {
+                this.currentMatch = 0;
             }
 
             Emitter.emit('docs-content-updated');
+        },
+        onNextMatch() {
+            if (this.totalMatches) {
+                this.currentMatch++;
+
+                if (this.currentMatch > this.totalMatches) {
+                    this.currentMatch = 1;
+                }
+
+                this.totalMatches = null;
+
+                const docsContent = document.querySelector('#documentation-content');
+                docsContent.innerHTML = this.docsHtml;
+
+                this.searchChildNodes(docsContent.childNodes);
+            }
+        },
+        onPreviousMatch() {
+            if (this.totalMatches) {
+                this.currentMatch--;
+
+                if (this.currentMatch <= 0) {
+                    this.currentMatch = this.totalMatches;
+                }
+
+                this.totalMatches = null;
+
+                const docsContent = document.querySelector('#documentation-content');
+                docsContent.innerHTML = this.docsHtml;
+
+                this.searchChildNodes(docsContent.childNodes);
+            }
         },
     },
 }
@@ -68,7 +147,22 @@ export default {
             </svg>
         </div>
         <div>
-            <input v-debounce:200ms.fireonempty="onSearch" debounce-events="input" style="width:14.05rem;" class="border rounded py-1 px-3 bg-white text-gray-700 focus:outline-none focus:shadow-outline" id="docs-search-input" type="search" placeholder="Search Documentation" />
+            <input ref="searchRef" v-debounce:200ms.fireonempty="onSearch" debounce-events="input" style="width:14.05rem;" class="border rounded py-1 px-3 bg-white text-gray-700 focus:outline-none focus:shadow-outline" id="docs-search-input" type="search" placeholder="Search Documentation" />
+        </div>
+        <div v-if="totalMatches !== null" class="py-1 absolute" style="left:18rem;">
+            {{ currentMatch }} of {{ totalMatches }}
+        </div>
+        <div v-if="totalMatches !== null" class="py-1 px-3 flex items-center relative left-24">
+            <span class="filter hover:invert-50 cursor-pointer py-1" @click="onNextMatch()">
+                <svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.292893 0.292893C0.683417 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L7 5.58579L12.2929 0.292893C12.6834 -0.0976311 13.3166 -0.0976311 13.7071 0.292893C14.0976 0.683418 14.0976 1.31658 13.7071 1.70711L7.70711 7.70711C7.31658 8.09763 6.68342 8.09763 6.29289 7.70711L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683418 0.292893 0.292893Z" fill="#25282B"/>
+                </svg>
+            </span>
+            <span class="filter hover:invert-50 transform rotate-180 ml-2 cursor-pointer py-1" @click="onPreviousMatch()">
+                <svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.292893 0.292893C0.683417 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L7 5.58579L12.2929 0.292893C12.6834 -0.0976311 13.3166 -0.0976311 13.7071 0.292893C14.0976 0.683418 14.0976 1.31658 13.7071 1.70711L7.70711 7.70711C7.31658 8.09763 6.68342 8.09763 6.29289 7.70711L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683418 0.292893 0.292893Z" fill="#25282B"/>
+                </svg>
+            </span>
         </div>
     </div>
 </template>
