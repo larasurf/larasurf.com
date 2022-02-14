6b74e19b3fd751bbbea26411fbd09a552a8a742c
Brice Hartmann <brice@bricehartmann.com>
2022-02-14 13:59:36 -0600

    safari home page issues

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
     <div class="relative mb-12 lg:mb-18">
-        <div class="flex justify-center mt-16 lg:mt-48 mb-32 lg:mb-48">
-            <div class="text-center absolute">
+        <div class="flex justify-center mt-16 lg:mt-48">
+            <div class="text-center">
                 <h1 class="text-3xl lg:text-5xl font-medium">Build with LaraSurf</h1>
-                <div class="text-md lg:text-2xl mt-6 mx-3 lg:mx-6">
+                <div class="text-md lg:text-2xl mt-6 mx-3 lg:mx-6" style="min-height:3rem;">
                     <div id="home-typer"></div>
                 </div>
             </div>
         </div>
-        <div class="flex justify-center mt-12 lg:mt-18">
+        <div class="flex justify-center mt-6">
             <div class="flex flex-wrap w-full lg:w-3/5 items-center justify-center">
                 <div class="w-1/3 flex items-center justify-center mt-3 md:mt-0">
                     <div class="hero-icon generate absolute bg-white border border-black rounded-full p-8 mb-6 filter transition hover:invert hover:bg-white"></div>
