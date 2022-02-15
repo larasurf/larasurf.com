491429a0721edf5841415c33ca8937a9d70c2360
Brice Hartmann <brice@bricehartmann.com>
2022-02-15 14:05:34 -0600

    add sitemap meta link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('js/fonts.js') }}"></script>
 
