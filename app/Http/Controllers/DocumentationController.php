f046dbb553eb0f5ea62248a0e7e42133bdf2b27c
Brice Hartmann <brice@bricehartmann.com>
2022-08-14 01:02:48 -0500

    docs typo

--- app/Http/Controllers/DocumentationController.php
+++ app/Http/Controllers/DocumentationController.php
@@ -25,7 +25,7 @@ class DocumentationController extends Controller
                     ],
                     [
                         'type' => 'paragraph',
-                        'html' => 'Your journey of rapidly implementing, deploying, and iterating on your next Laravel project begins here.',
+                        'html' => 'Your journey of rapidly implementing, deploying, and iterating upon your next Laravel project begins here.',
                     ],
                     [
                         'type' => 'callout',
